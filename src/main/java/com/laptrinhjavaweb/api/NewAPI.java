07267229adc886e046405383007a03c2713b1294
bachtung144 <bachtung144007@gmail.com>
2021-03-03 17:22:36 +0700

    first commit

---
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -0,0 +1,24 @@
+package com.laptrinhjavaweb.api;
+
+import com.laptrinhjavaweb.dto.NewDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NewAPI {
+
+    @PostMapping(value = "/new")
+
+    public NewDTO createNew(@RequestBody NewDTO model) {
+        return model;
+    }
+
+    @PutMapping(value = "/new")
+    public NewDTO updateNew(@RequestBody NewDTO model) {
+        return model;
+    }
+
+    @DeleteMapping(value = "/new")
+    public void deleteNew(@RequestBody long[] ids) {
+    }
+
+}
