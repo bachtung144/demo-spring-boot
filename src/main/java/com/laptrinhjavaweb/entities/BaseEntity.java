2f020ccbc41deaee1a80029614752233f732d10b
bachtung144 <bachtung144007@gmail.com>
2021-03-04 22:03:06 +0700

    add entities

---
+++ src/main/java/com/laptrinhjavaweb/entities/BaseEntity.java
@@ -0,0 +1,60 @@
+package com.laptrinhjavaweb.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id // not null + primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+    private long id;
+
+    @Column
+    private String createdBy;
+
+    @Column
+    private Date createdDate;
+
+    @Column
+    private String modifiedBy;
+
+    @Column
+    private Date modifiedDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
---
+++ src/main/java/com/laptrinhjavaweb/entities/NewEntity.java
@@ -0,0 +1,55 @@
+package com.laptrinhjavaweb.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "news")
+public class NewEntity extends BaseEntity{
+
+    @Column
+    private String title;
+
+    @Column
+    private String thumbnail;
+
+    @Column
+    private String shortDescription;
+
+    @Column
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
